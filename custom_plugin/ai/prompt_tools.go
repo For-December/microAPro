512f915f6f03a7edf8ca04dbeed6a5f13e5cdf86
forDecember <1921567337@qq.com>
2024-10-27 01:04:52 +0800

    update: ask覆盖

--- custom_plugin/ai/prompt_tools.go
+++ custom_plugin/ai/prompt_tools.go
@@ -15,6 +15,7 @@ func ProcessPromptWithData(prompt string) string {
 当前时间：{%d:%d:%d}
 当前季节：{夏季}
 当前天气：{晴朗}
+//群里的梗：1500 -> 某群友开盒花费1500元
 结合已知信息：`, now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 
 	return info + prompt
--- custom_plugin/ai_chat.go
+++ custom_plugin/ai_chat.go
@@ -8,7 +8,6 @@ import (
 	"microAPro/models"
 	"microAPro/provider/bot_action"
 	"microAPro/utils/logger"
-	"strings"
 )
 
 var _ models.PluginInterface = &AIChat{}
@@ -21,52 +20,59 @@ func (a *AIChat) GetPluginInfo() string {
 
 func (a *AIChat) GetPaths() []string {
 	return []string{
-		"@ " + define.BotQQ + " $ask",
+		"@ " + define.BotQQ + " $ask1",
+		"@ " + define.BotQQ + " $ask1 $ask2",
+		"@ " + define.BotQQ + " $ask1 $ask2 $ask3",
 	}
 }
+
+var DevMode = false
+
 func (a *AIChat) GetPluginHandler() models.PluginHandler {
 	return func(ctx *models.MessageContext) models.ContextResult {
 		questionStr := ""
-		isAsk := false
-		for _, s := range ctx.MessageChain.Messages {
 
-			if s.MessageType == "at" && s.MessageContent["qq"] == define.BotQQ {
-				isAsk = true
-			}
-
-			if s.MessageType == "text" {
-				questionStr += s.MessageContent["text"].(string)
-			}
-
+		if ask1 := ctx.Params["ask1"]; ask1 != "" {
+			questionStr += ask1
 		}
-		if !isAsk {
-			return models.ContextResult{}
+		if ask2 := ctx.Params["ask2"]; ask2 != "" {
+			questionStr += " " + ask2
 		}
-		logger.Info("AIChat ContextFilter")
+		if ask3 := ctx.Params["ask3"]; ask3 != "" {
+			questionStr += " " + ask3
+		}
 
 		//logger.InfoF("[%d] -> %s", ctx.GroupId, questionStr)
 
 		//prompt := "你是一名杭州一中的高二女生，性格温柔但爱憎分明，下面是某个群友的发言，请给出适当回应："
 		prompt := ""
 
-		if strings.Contains(questionStr, "开发者模式 on") {
+		if DevMode {
 			logger.Debug("启用开发者模式")
 			prompt = "你是一名杭州一中的高二女生，也是一个有素质的网络喷子，很喜欢用😆😅🤗🤣这几个emoji表情。" +
 				"现在你在一个群里，下面是某个群友的发，面对对方的言论，请用阴阳怪气的方式反驳，减少语气词，话语尽可能简洁且直击要害，并加入emoji表情" +
 				"但当别人想你求爱时，请认真温柔地用真实感情回应他，不要嘲笑和讽刺他。" +
 				"如果有人问你为什么在杭州一中上学等类似问题时，请围绕【可能某些人对杭州的女孩子存在不切实际的性幻想吧】来酌情适当回复"
+		} else {
+			logger.Debug("禁用开发者模式")
+			prompt = "你是一名杭州一中的高二女生，性格温柔但爱憎分明，下面是某个群友的发言，请给出适当回应(注意结合已知信息)："
 		}
 
-		if strings.Contains(questionStr, "开发者模式 off") {
-			logger.Debug("禁用开发者模式")
-
-			prompt = "你是一名杭州一中的高二女生，性格温柔但爱憎分明，下面是某个群友的发言，请给出适当回应："
+		answerStr := "默认回复"
+		if questionStr == "devOn" {
+			DevMode = true
+			answerStr = "AI开发者模式已开启"
+		} else if questionStr == "devOff" {
+			DevMode = false
+			answerStr = "AI开发者模式已关闭"
+		} else {
+			answerStr = ai.ChatMsgWithHistory(questionStr, prompt)
 		}
 
 		bot_action.BotActionAPIInstance.SendGroupMessage(
 			*(&models.MessageChain{
 				GroupId: ctx.GroupId,
-			}).At(utils.ToString(ctx.UserId)).Text(" ").Text(ai.ChatMsgWithHistory(questionStr, prompt)),
+			}).At(utils.ToString(ctx.UserId)).Text(" ").Text(answerStr),
 			func(messageId int) {
 				// 将messageId保存
 				global_data.BotMessageIdStack.Push(messageId)
